cdbf235ebaf395e9ec664328dfd936dd12fbafee
sh <sh@192.168.0.5>
2013-07-09 16:32:03 +0900

    Hi hellow Everyone

--- practice/src/practicef.java
+++ practice/src/practicef.java
@@ -2,6 +2,7 @@
 public class practicef {
 static public void main(String[] args){
 	System.out.println("임시");
-	System.out.println("dddd");
+	System.out.println("성환아 공부좀 열심히해바");
+	System.out.println("ㅇㅇㅇㅇㅇㅇㅇㅇ");
 }
 }
